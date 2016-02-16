c5b46b76fed87e7cb4f90458a8b569ff37771852
Benjamin Reissaus <benjamin.reissaus@student.hpi.uni-potsdam.de>
2016-02-16 11:04:40 +0100

    retrieve city name from command line argument

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,8 +3,15 @@ import java.util.List;
 public class Main {
 
     public static void main(String[] args) {
+
+        if (args.length != 1) {
+            System.out.println("Please provide a city name as an argument.");
+            System.exit(1);
+        }
+        String cityName = args[0];
+
         GoEuroApiClient apiClient = new GoEuroApiClient();
-        List<City> cities = apiClient.sendQuery("Berlin");
+        List<City> cities = apiClient.sendQuery(cityName);
         CSVExporter csvExporter = new CSVExporter();
         csvExporter.write(cities);
     }
